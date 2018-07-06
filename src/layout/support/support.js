3c5b7e99146ccc9782d77a8d4746123ec2296e84
Waschnick <swaschni@XIN851573.asv.local>
2018-07-06 16:12:11 +0200

    WIP

---
+++ src/layout/support/support.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+require('./support.scss');
+
+
+class Support extends React.Component {
+  render() {
+    return (
+      <div className="support section">
+        <div className="support__titles section-content">
+          <h6>SUPPORT</h6>
+          <h2>Contact & Additional Information</h2>
+          <p>
+            Want to know more about OIL before becoming an active collaborator?
+            <br/>In that case, please sign up to our weekly webinar!
+          </p>
+        </div>
+        <div className="support-content section-content">
+
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Support;
