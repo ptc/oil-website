66b02a15d1399ee493ab5ed3c83c89d3862a4341
Dominique Klevinghaus <adk@codekommando.de>
2018-07-11 15:35:41 +0200

    Happy linting

--- src/layout/button/link-button.js
+++ src/layout/button/link-button.js
@@ -5,15 +5,15 @@ class LinkButton extends React.Component {
   render() {
 
     let href = this.props.href;
-    if(!href && this.props.text == 'Sandbox') {
+    if(!href && this.props.text === 'Sandbox') {
       href = 'http://sandbox.oiljs.org';
     }
-    if(!href && this.props.text == 'Github') {
+    if(!href && this.props.text === 'Github') {
       href = 'https://github.com/as-ideas/oil';
     }
 
     return (
-        <a className={this.props.cssClass} href={href} target="_blank">
+      <a className={this.props.cssClass} href={href} target="_blank">
         {this.props.text}
       </a>
     )
